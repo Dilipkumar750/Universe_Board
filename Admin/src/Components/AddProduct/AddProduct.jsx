24d668dbda824264ab86172b1809617344c88f36
Dilipkumar750 <dilipk5406@gmail.com>
2024-10-30 15:48:42 +0530

    changes on admin page

--- Admin/src/Components/AddProduct/AddProduct.jsx
+++ Admin/src/Components/AddProduct/AddProduct.jsx
@@ -8,60 +8,52 @@ const AddProduct = () => {
   const dispatch = useDispatch();
   const { isLoading } = useSelector((state) => state.product.addProduct);
 
-  const [image, setImage] = useState(null); 
+  const [image, setImage] = useState(null);
   const [productDetails, setProductDetails] = useState({
     title: "",
     description: "",
     category: "",
     subCategory: "",
   });
-
-  const imageHandler = (event) => {
-    const file = event.target.files[0];
-
-    if (!file) {
-      alert("No file selected.");
-      return;
-    }
-
-  
-    const allowedTypes = ["image/png", "image/jpg", "image/jpeg", "image/svg+xml"];
-    if (!allowedTypes.includes(file.type)) {
-      alert("Only PNG, JPG, JPEG, and SVG files are allowed.");
-      return;
-    }
-
-    const img = new Image();
-    img.src = URL.createObjectURL(file);
-
-    img.onload = () => {
-      setImage(file);
-    };
-  };
-
-  const addProductHandler = async () => {
-    dispatch(addProduct({ productDetails, image }));
-  };
-
-  const changeHandler = (e) => {
+  const [details, setDetails] = useState([{ name: "", value: "" }]);
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
+    setProductDetails((prevDetails) => ({
+      ...prevDetails,
+      [name]: value,
+    }));
+  };
 
-    const alphanumericRegex = /^[a-zA-Z0-9\s]*$/;
-    const descriptionRegex = /^[a-zA-Z0-9\s.,?]*$/;
-
-    if (name === "description") {
-      if (descriptionRegex.test(value)) {
-        setProductDetails({ ...productDetails, [name]: value });
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const allowedTypes = ["image/png", "image/jpg", "image/jpeg", "image/svg+xml"];
+      if (allowedTypes.includes(file.type)) {
+        setImage(file);
       } else {
-        alert("Only letters, numbers, and basic punctuation are allowed in the description.");
-      }
-    } else {
-      if (alphanumericRegex.test(value)) {
-        setProductDetails({ ...productDetails, [name]: value });
-      } else {
-        alert("Only letters and numbers are allowed.");
+        alert("Only PNG, JPG, JPEG, and SVG files are allowed.");
       }
     }
+  };
+
+  const handleAddDetail = () => {
+    setDetails([...details, { name: "", value: "" }]);
+  };
+
+  const handleDetailChange = (index, event) => {
+    const values = [...details];
+    values[index][event.target.name] = event.target.value;
+    setDetails(values);
+  };
+
+  const handleDeleteDetail = (index) => {
+    const values = [...details];
+    values.splice(index, 1); // Remove the detail at the specified index
+    setDetails(values);
+  };
+  const handleSubmit = () => {
+    // dispatch(addProduct({ productDetails, image, details }));
+    console.log(productDetails, image, details);
   };
 
   const categories = {
@@ -151,106 +143,133 @@ const AddProduct = () => {
       "Digital Library"
     ]
   };
-  
+
   return (
     <div className="addproduct">
+      <h1>Add Product</h1>
+
       <div className="addproduct-itemfield">
-        <p>
-          Product Title <span style={{ color: "red" }}>*</span>
-        </p>
+        <label htmlFor="title">Product Title</label>
         <input
           type="text"
+          id="title"
           name="title"
           value={productDetails.title}
-          onChange={changeHandler}
-          placeholder="Type here"
-          required
+          onChange={handleInputChange}
         />
       </div>
 
-
       <div className="addproduct-itemfield">
-        <p>Description <span style={{ color: "red" }}>*</span>
-        </p>
-        <textarea 
+        <label htmlFor="description">Product Description</label>
+        <textarea
+          id="description"
           name="description"
           value={productDetails.description}
-          onChange={changeHandler}
-          placeholder="Type here"
-          style={{ height: "200px", width: "100%" }}
+          onChange={handleInputChange}
         />
       </div>
 
-      <div
-        className="addproduct-itemfield"
-        style={{ display: "flex", justifyContent: "space-between" }}
-      >
-        <div style={{ marginRight: "20px", flex: "1" }}>
-          <p style={{ marginBottom: "5px" }}>Product Category <span style={{ color: "red" }}>*</span> 
-          </p>
-          <select
-            value={productDetails.category}
-            name="category"
-            className="add-product-selector"
-            onChange={changeHandler}
-            style={{ width: "100%", minWidth: "150px" }}
-          >
-            <option value="">Select Category</option>
-            {Object.keys(categories).map((cat) => (
-              <option key={cat} value={cat}>
-                {cat}
-              </option>
-            ))}
-          </select>
-        </div>
-
-      
-        <div style={{ flex: "1" }}>
-          <p style={{ marginBottom: "5px" }}>Product SubCategory</p>
-          <select
-            value={productDetails.subCategory}
-            name="subCategory"
-            className="add-product-selector"
-            onChange={changeHandler}
-            style={{ width: "100%", minWidth: "150px" }}
-            disabled={!productDetails.category}
-          >
-            <option value="">Select SubCategory</option>
-            {categories[productDetails.category]?.map((subcat) => (
-              <option key={subcat} value={subcat}>
-                {subcat}
-              </option>
-            ))}
-          </select>
-        </div>
-      </div>
       <div className="addproduct-itemfield">
-        <p>Upload Image <span style={{ color: "red" }}>*</span>
-        </p>
-        <label htmlFor="file-input">
+        <label htmlFor="category">Product Category</label>
+        <select
+          id="category"
+          name="category"
+          value={productDetails.category}
+          onChange={handleInputChange}
+        >
+          <option value="">Select Category</option>
+          {Object.keys(categories).map((cat) => (
+            <option key={cat} value={cat}>
+              {cat}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div className="addproduct-itemfield">
+        <label htmlFor="subCategory">Sub Category</label>
+        <select
+          id="subCategory"
+          name="subCategory"
+          value={productDetails.subCategory}
+          onChange={handleInputChange}
+          disabled={!productDetails.category}
+        >
+          <option value="">Select SubCategory</option>
+          {categories[productDetails.category]?.map((subcat) => (
+            <option key={subcat} value={subcat}>
+              {subcat}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      {/* Product Details Section */}
+      <div className="addproduct-itemfield">
+        <label>Product Details</label>
+        
+        {details.map((detail, index) => (
+          <div key={index} className="d-flex mb-2">
+            <div className="flex-1">{index + 1}</div>
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              className="flex-1 border border-gray-300 rounded p-2"
+              value={detail.name}
+              onChange={(e) => handleDetailChange(index, e)}
+            />
+            <input
+              type="text"
+              name="value"
+              placeholder="Value"
+              className="flex-1 border border-gray-300 rounded p-2"
+              value={detail.value}
+              onChange={(e) => handleDetailChange(index, e)}
+            />
+            <button
+              type="button"
+              onClick={handleAddDetail}
+              className="text-blue-500 font-bold"
+            >
+              +
+            </button>
+            <button
+              type="button"
+              onClick={() => handleDeleteDetail(index)}
+              className="text-red-500" // Style for delete button
+            >
+              -
+            </button>
+          </div>
+        ))}
+      </div>
+
+      {/* Image Upload Section */}
+      <div className="addproduct-itemfield">
+        <label htmlFor="file-input">Upload Image</label>
+        <label htmlFor="file-input" className="cursor-pointer">
           <img
             className="addproduct-thumbnail-img"
             src={!image ? upload_area : URL.createObjectURL(image)}
             alt="Upload area"
           />
         </label>
-        <input
-          onChange={imageHandler}
-          type="file"
-          name="image"
-          id="file-input"
-          hidden
-        />
+        <input id="file-input" type="file" onChange={handleFileChange} hidden />
+        {image && <p>{image.name}</p>}
       </div>
 
       <button
+        onClick={handleSubmit}
         className="addproduct-btn"
-        onClick={addProductHandler} 
         disabled={isLoading}
       >
-        {isLoading ? "Adding..." : "ADD"}
+        {isLoading ? "Adding..." : "Add Product"}
       </button>
     </div>
+
+
+
   );
 };
 
