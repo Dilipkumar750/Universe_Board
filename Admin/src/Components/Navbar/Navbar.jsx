2c465563a89958dba605daefef95e136b2435aa4
Dilipkumar750 <dilipk5406@gmail.com>
2024-11-26 12:41:20 +0530

    dilip changes

--- Admin/src/Components/Navbar/Navbar.jsx
+++ Admin/src/Components/Navbar/Navbar.jsx
@@ -5,10 +5,10 @@ import navprofileIcon from '../Assets/nav-profile.svg';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg shadow-sm mb-1" style={{ backgroundColor: 'black', maxHeight: '100px' }}>
+    <nav className="navbar navbar-expand-lg shadow-sm mb-1" style={{ backgroundColor: [rgb(3, 3, 164)], maxHeight: '100px' }}>
       <div className="container d-flex justify-content-center align-items-center">
         <a href="/" className="navbar-brand d-flex align-items-center">
-          <img src={navlogo} className="img-fluid" style={{ width: '200px' }} alt="Logo" />
+          <img src={navlogo} className="img-fluid" style={{ width: '100px' }} alt="Logo" />
         </a>
       </div>
     </nav>
